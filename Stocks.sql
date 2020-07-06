0bba8bc7568fdda40ccf95129de0fdf5dfde8101
tal <taltole@gmail.com>
2020-07-06 20:50:45 +0300

    db final version

--- Stocks.sql
+++ Stocks.sql
@@ -1,103 +1,113 @@
 CREATE TABLE `Main` (
-  `Ticker` varchar(255) PRIMARY KEY AUTO_INCREMENT,
+  `id` double PRIMARY KEY AUTO_INCREMENT,
+  `Ticker` varchar(255),
   `Name` varchar(255),
-  `Last` int,
-  `Change_Percent` int,
+  `Last` float,
+  `Change_Percent` float,
   `Rating` varchar(255),
-  `Volume` int,
-  `Mkt_Cap` int,
-  `Price_to_Earnings` int,
-  `Employees` int,
+  `Volume` float,
+  `Mkt_Cap` float,
+  `Price_to_Earnings` float,
+  `Employees` float,
   `Sector` varchar(255)
 );
 
 CREATE TABLE `Industry` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `id` double PRIMARY KEY AUTO_INCREMENT,
   `Name` varchar(255),
-  `Mkt_Cap` int,
-  `Change_Percent` int,
-  `Vol` int,
+  `Mkt_Cap` float,
+  `Change_Percent` float,
+  `Vol` float,
   `Sector` varchar(255)
 );
 
 CREATE TABLE `Sectors` (
-  `Id` int PRIMARY KEY AUTO_INCREMENT,
+  `id` double PRIMARY KEY AUTO_INCREMENT,
   `Name` varchar(255),
-  `Mkt_Cap` int,
-  `Change_Percent` int,
-  `Vol` int
+  `Mkt_Cap` float,
+  `Change_Percent` float,
+  `Vol` float
 );
 
 CREATE TABLE `Valuation` (
-  `Ticker` varchar(255) PRIMARY KEY AUTO_INCREMENT,
-  `Market_Capitalization` int,
-  `Enterprise_Value` int,
-  `Enterprise_Value_EBITDA` int,
-  `Total_Shares_Outstanding` int,
-  `Number_Employees` int,
-  `Number_Shareholders` int,
-  `Price_to_Earnings` int,
-  `Price_to_Revenue` int,
-  `Price_Book` int,
-  `Price_Sales` int
+  `id` double PRIMARY KEY AUTO_INCREMENT,
+  `Ticker` varchar(255),
+  `Market_Capitalization` float,
+  `Enterprise_Value` float,
+  `Enterprise_Value_EBITDA` float,
+  `Total_Shares_Outstanding` float,
+  `Number_Employees` float,
+  `Number_Shareholders` float,
+  `Price_to_Earnings` float,
+  `Price_to_Revenue` float,
+  `Price_Book` float,
+  `Price_Sales` float
 );
 
 CREATE TABLE `Metrics` (
-  `Ticker` varchar(255) PRIMARY KEY,
-  `Return_on_Assets` int,
-  `Return_on_Equity` int,
-  `Return_on_Invested_Capital` int,
-  `Revenue_per_Employee` int
+  `id` double PRIMARY KEY AUTO_INCREMENT,
+  `Ticker` varchar(255),
+  `Return_on_Assets` float,
+  `Return_on_Equity` float,
+  `Return_on_Invested_Capital` float,
+  `Revenue_per_Employee` float
 );
 
 CREATE TABLE `Balance_Sheet` (
-  `Ticker` varchar(255) PRIMARY KEY,
-  `Quick_Ratio` int,
-  `Current_Ratio` int,
-  `Debt_to_Equity` int,
-  `Net_Debt` int,
-  `Total_Debt` int,
-  `Total_Assets` int
+  `id` double PRIMARY KEY AUTO_INCREMENT,
+  `Ticker` varchar(255),
+  `Quick_Ratio` float,
+  `Current_Ratio` float,
+  `Debt_to_Equity` float,
+  `Net_Debt` float,
+  `Total_Debt` float,
+  `Total_Assets` float
 );
 
 CREATE TABLE `Price_History` (
-  `Ticker` varchar(255) PRIMARY KEY,
-  `Average_Volume_10d` int,
-  `1_Year_beta` int,
-  `52_Week_High` int,
-  `52_Week_Low` int
+  `id` double PRIMARY KEY AUTO_INCREMENT,
+  `Ticker` varchar(255),
+  `Average_Volume_10d` float,
+  `1_Year_beta` float,
+  `52_Week_High` float,
+  `52_Week_Low` float
 );
 
 CREATE TABLE `Dividends` (
-  `Ticker` varchar(255) PRIMARY KEY,
-  `Dividends_Paid` int,
-  `Dividends_Yield` int,
-  `Dividends_per_Share` int
+  `id` double PRIMARY KEY AUTO_INCREMENT,
+  `Ticker` varchar(255),
+  `Dividends_Paid` float,
+  `Dividends_Yield` float,
+  `Dividends_per_Share` float
 );
 
 CREATE TABLE `Margins` (
-  `Ticker` varchar(255) PRIMARY KEY,
-  `Net_Margin` int,
-  `Gross_Margin` int,
-  `Operating_Margin` int,
-  `Pretax_Margin` int
+  `id` double PRIMARY KEY AUTO_INCREMENT,
+  `Ticker` varchar(255),
+  `Net_Margin` float,
+  `Gross_Margin` float,
+  `Operating_Margin` float,
+  `Pretax_Margin` float
 );
 
 CREATE TABLE `Income` (
-  `Ticker` varchar(255) PRIMARY KEY,
-  `Basic_EPS_FY` int,
-  `Basic_EPS_TTM` int,
-  `EPS_Diluted` int,
-  `Net_Income` int,
-  `EBITDA` int,
-  `Gross_Profit_MRQ` int,
-  `Gross_Profit_FY` int,
-  `Last_Year_Revenue` int,
-  `Total_Revenue` int,
-  `Free_Cash_Flow` int
+  `id` double PRIMARY KEY AUTO_INCREMENT,
+  `Ticker` varchar(255),
+  `Basic_EPS_FY` float,
+  `Basic_EPS_TTM` float,
+  `EPS_Diluted` float,
+  `Net_Income` float,
+  `EBITDA` float,
+  `Gross_Profit_MRQ` float,
+  `Gross_Profit_FY` float,
+  `Last_Year_Revenue` float,
+  `Total_Revenue` float,
+  `Free_Cash_Flow` float
 );
 
-ALTER TABLE `Main` ADD FOREIGN KEY (`Sector`) REFERENCES `Sectors` (`Id`);
+ALTER TABLE `Main` ADD FOREIGN KEY (`Sector`) REFERENCES `Sectors` (`id`);
+
+ALTER TABLE `Industry` ADD FOREIGN KEY (`Sector`) REFERENCES `Sectors` (`id`);
 
 ALTER TABLE `Industry` ADD FOREIGN KEY (`Sector`) REFERENCES `Sectors` (`Name`);
 
