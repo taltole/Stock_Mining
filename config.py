1714bb0101cbb962a5cb86c20dcad2a8474ffe6b
tal <taltole@gmail.com>
2020-08-01 14:57:37 +0300

    cd changed

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+chrome_options.add_argument('start-maximized')
+chrome_options.add_argument('disable-infobars')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument('disable-infobars')
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument('start-maximized')
 chrome_options.headless = True
 
 desired_width = 320
